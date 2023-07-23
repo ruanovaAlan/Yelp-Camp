053083d41490a97553e337fb0aea089ab3ea7ba6
Alan Ruanova <ruanovaalan96@gmail.com>
2023-07-23 12:26:22 -0600

    Add middleware for login when register

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registerUser = await User.register(user, password);
-        req.flash('success', 'Welcome to YelpCamp');
-        res.redirect('/campgrounds');
+        req.login(registerUser, err => { //middleware to login when register
+            if (err) return next(err);
+            req.flash('success', 'Welcome to YelpCamp');
+            res.redirect('/campgrounds');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
@@ -29,6 +32,16 @@ router.get('/login', (req, res) => {
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome Back!');
     res.redirect('/campgrounds');
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye');
+        res.redirect('/campgrounds');
+    });
 })
 
 module.exports = router;
