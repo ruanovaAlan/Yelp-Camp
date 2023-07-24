ca416fd63ddd9e72681d2cdde0a60e9cac45ce52
Alan Ruanova <ruanovaalan96@gmail.com>
2023-07-24 17:44:09 -0600

    Add author to campground Schema

--- seeds/index.js
+++ seeds/index.js
@@ -20,8 +20,9 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random()*20 + 10);
+        const price = Math.floor(Math.random() * 20 + 10);
         const camp = new Campground({
+            author: '64bd6bc31b7395358f470389',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
